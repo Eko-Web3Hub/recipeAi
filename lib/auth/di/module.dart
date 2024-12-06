ae796e6b6db99ddac7998c1d6672f32317fc52f2
imdad <imdadadelabou0@gmail.com>
2024-12-06 01:15:50 +0100

    [FEAT] user info repository is ready

--- lib/auth/di/module.dart
+++ lib/auth/di/module.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:recipe_ai/auth/application/auth_service.dart';
 import 'package:recipe_ai/auth/application/auth_user_service.dart';
 import 'package:recipe_ai/auth/application/register_usecase.dart';
 import 'package:recipe_ai/auth/domain/repositories/user_personnal_info_repository.dart';
+import 'package:recipe_ai/auth/infrastructure/user_personnal_info_repository.dart';
 import 'package:recipe_ai/di/container.dart';
 import 'package:recipe_ai/di/module.dart';
 
@@ -12,7 +14,9 @@ class AuthModule implements IDiModule {
   @override
   void register(DiContainer di) {
     di.registerLazySingleton<IUserPersonnalInfoRepository>(
-      () => FirestoreUserPersonnalInfoRepository(),
+      () => FirestoreUserPersonnalInfoRepository(
+        di<FirebaseFirestore>(),
+      ),
     );
     di.registerFactory<IAuthService>(
       () => AuthService(
