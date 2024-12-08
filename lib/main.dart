523e47167c0e0bd3f2993fd789caef3917189c7b
imdad <imdadadelabou0@gmail.com>
2024-12-08 18:47:17 +0100

    [FEAT] redirect the user to the right app path according to the state of the Authentification

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'dart:developer';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:recipe_ai/auth/application/auth_user_service.dart';
+import 'package:recipe_ai/auth/presentation/auth_navigation_controller.dart';
 import 'package:recipe_ai/di/container.dart';
 import 'package:recipe_ai/di/module.dart';
 import 'package:recipe_ai/firebase_options.dart';
@@ -36,28 +41,43 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Recipe AI',
-      theme: ThemeData(
-        primaryColor: const Color(0xff129575),
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        textTheme: TextTheme(
-          displayLarge: GoogleFonts.poppins(
-            fontWeight: FontWeight.w600,
-            fontSize: 20.0,
-            height: 30 / 20,
-            color: Colors.black,
-          ),
-          labelSmall: GoogleFonts.poppins(
-            fontWeight: FontWeight.w400,
-            fontSize: 11.0,
-            height: 16.5 / 11,
-            color: const Color(0xffA9A9A9),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => AuthNavigationController(
+            di<IAuthUserService>(),
           ),
         ),
-        useMaterial3: true,
+      ],
+      child: BlocListener<AuthNavigationController, AuthNavigationState>(
+        listener: (context, state) {
+          log('AuthNavigationState: $state');
+          _router.refresh();
+        },
+        child: MaterialApp.router(
+          title: 'Recipe AI',
+          theme: ThemeData(
+            primaryColor: const Color(0xff129575),
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            textTheme: TextTheme(
+              displayLarge: GoogleFonts.poppins(
+                fontWeight: FontWeight.w600,
+                fontSize: 20.0,
+                height: 30 / 20,
+                color: Colors.black,
+              ),
+              labelSmall: GoogleFonts.poppins(
+                fontWeight: FontWeight.w400,
+                fontSize: 11.0,
+                height: 16.5 / 11,
+                color: const Color(0xffA9A9A9),
+              ),
+            ),
+            useMaterial3: true,
+          ),
+          routerConfig: _router,
+        ),
       ),
-      routerConfig: _router,
     );
   }
 }
--- lib/nav/router.dart
+++ lib/nav/router.dart
@@ -1,10 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
+import 'package:recipe_ai/auth/presentation/auth_navigation_controller.dart';
 import 'package:recipe_ai/auth/presentation/login_view.dart';
 import 'package:recipe_ai/auth/presentation/register/register_view.dart';
 import 'package:recipe_ai/home/presentation/home_screen.dart';
 import 'package:recipe_ai/nav/splash_screen.dart';
 import 'package:recipe_ai/user_preferences/presentation/user_preferences_view.dart';
+
+FutureOr<String?> _guardAuth(BuildContext context, GoRouterState state) {
+  final authState = context.read<AuthNavigationController>().state;
+
+  switch (authState) {
+    case AuthNavigationState.loading:
+      return '/';
+    case AuthNavigationState.loggedIn:
+      return null;
+    case AuthNavigationState.loggedOut:
+      return '/login';
+  }
+}
 
 GoRouter createRouter() => GoRouter(
       initialLocation: '/',
@@ -16,7 +33,14 @@ GoRouter createRouter() => GoRouter(
           path: '/',
           builder: (context, state) => const SplashScreen(),
           redirect: (BuildContext context, _) {
-            return '/login';
+            final authState = context.read<AuthNavigationController>().state;
+            if (authState == AuthNavigationState.loading) {
+              return null;
+            }
+
+            return authState == AuthNavigationState.loggedIn
+                ? '/home'
+                : '/login';
           },
         ),
         GoRoute(
@@ -37,6 +61,7 @@ GoRouter createRouter() => GoRouter(
         GoRoute(
           name: 'Home',
           path: '/home',
+          redirect: _guardAuth,
           builder: (context, state) => const HomeScreen(),
         ),
       ],
