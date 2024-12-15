898ecd9112a27668deab975daf646b305d705672
spencer2k19 <hachemeloic@gmail.com>
2024-12-15 23:21:58 +0100

    [FEAT] Work on Receipt module

---
+++ lib/receipe/di/module.dart
@@ -0,0 +1,24 @@
+import 'package:recipe_ai/auth/application/auth_user_service.dart';
+import 'package:recipe_ai/di/container.dart';
+import 'package:recipe_ai/di/module.dart';
+import 'package:recipe_ai/receipe/application/retrieve_receipe_from_api_one_time_per_day_usecase.dart';
+import 'package:recipe_ai/receipe/domain/repositories/user_receipe_repository.dart';
+import 'package:recipe_ai/receipe/infrastructure/receipe_repository.dart';
+
+class ReceipeModule implements IDiModule {
+  const ReceipeModule();
+
+  @override
+  void register(DiContainer di) {
+    di.registerLazySingleton<IUserReceipeRepository>(
+      () => UserReceipeRepository(),
+    );
+
+    di.registerFactory(
+      () => RetrieveReceipeFromApiOneTimePerDayUsecase(
+        di<IUserReceipeRepository>(),
+        di<IAuthUserService>(),
+      ),
+    );
+  }
+}
