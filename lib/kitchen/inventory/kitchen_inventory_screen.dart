e6001abc0374ff5ebc846c05981038234414fd61
imdad <imdadadelabou0@gmail.com>
2024-12-19 04:23:19 +0100

    [FEAT] Finish implement Add invoice picture view

--- lib/kitchen/inventory/kitchen_inventory_screen.dart
+++ lib/kitchen/inventory/kitchen_inventory_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
+import 'package:go_router/go_router.dart';
 import 'package:recipe_ai/utils/app_text.dart';
 import 'package:recipe_ai/utils/colors.dart';
 import 'package:recipe_ai/utils/constant.dart';
@@ -11,23 +15,33 @@ class KitchenInventoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const SafeArea(
+    return SafeArea(
       child: Scaffold(
-        appBar: KitchenInventoryAppBar(),
-        body: Column(
-          children: [
-            Gap(10.0),
-            _EmptyKitchenInventoryView(),
-          ],
+        appBar: KitchenInventoryAppBar(
+          arrowLeftOnPressed: () => context.go('/home'),
+        ),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              Gap(10.0),
+              _EmptyKitchenInventoryView(),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
+/// The appBar for every screen in the kitchen inventory section
 class KitchenInventoryAppBar extends StatelessWidget
     implements PreferredSizeWidget {
-  const KitchenInventoryAppBar({super.key});
+  const KitchenInventoryAppBar({
+    super.key,
+    this.arrowLeftOnPressed,
+  });
+
+  final VoidCallback? arrowLeftOnPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +54,10 @@ class KitchenInventoryAppBar extends StatelessWidget
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          SvgPicture.asset('assets/images/arrow-black-left.svg'),
+          InkWell(
+            onTap: arrowLeftOnPressed,
+            child: SvgPicture.asset('assets/images/arrow-black-left.svg'),
+          ),
           const Gap(25),
           Text(
             AppText.yourKitchenInventory,
@@ -55,8 +72,30 @@ class KitchenInventoryAppBar extends StatelessWidget
   Size get preferredSize => const Size.fromHeight(70);
 }
 
-class _EmptyKitchenInventoryView extends StatelessWidget {
+class _EmptyKitchenInventoryView extends StatefulWidget {
   const _EmptyKitchenInventoryView();
+
+  @override
+  State<_EmptyKitchenInventoryView> createState() =>
+      _EmptyKitchenInventoryViewState();
+}
+
+class _EmptyKitchenInventoryViewState
+    extends State<_EmptyKitchenInventoryView> {
+  File? _receiptPicture;
+
+  void _uploadReceiptPicture() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['jpg', 'jpeg', 'png'],
+    );
+
+    if (result != null) {
+      setState(() {
+        _receiptPicture = File(result.files.single.path!);
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +117,14 @@ class _EmptyKitchenInventoryView extends StatelessWidget {
           decoration: BoxDecoration(
             color: greyVariantColor,
             borderRadius: BorderRadius.circular(10),
+            image: _receiptPicture != null
+                ? DecorationImage(
+                    image: FileImage(
+                      _receiptPicture!,
+                    ),
+                    fit: BoxFit.cover,
+                  )
+                : null,
           ),
         ),
         const Gap(21),
@@ -99,7 +146,43 @@ class _EmptyKitchenInventoryView extends StatelessWidget {
             color: Theme.of(context).primaryColor,
           ),
         ),
+        const Gap(11),
+        KitchenInventoryActionWidget(
+          icon: 'assets/images/cameraLogo.svg',
+          onPressed: _uploadReceiptPicture,
+        ),
       ],
+    );
+  }
+}
+
+class KitchenInventoryActionWidget extends StatelessWidget {
+  const KitchenInventoryActionWidget({
+    super.key,
+    required this.icon,
+    this.onPressed,
+  });
+
+  final String icon;
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        width: 48,
+        height: 48,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor,
+          shape: BoxShape.circle,
+        ),
+        child: Center(
+          child: SvgPicture.asset(
+            icon,
+          ),
+        ),
+      ),
     );
   }
 }
