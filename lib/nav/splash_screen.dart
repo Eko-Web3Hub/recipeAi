c47d9b9f6ce27439b32657b6c494c9dd57fb1819
imdad <imdadadelabou0@gmail.com>
2024-12-27 18:14:56 +0100

    [FEAT] update icon app

--- lib/nav/splash_screen.dart
+++ lib/nav/splash_screen.dart
@@ -5,9 +5,11 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('RECIPE AI'),
+        child: Image.asset(
+          'assets/images/logo.png',
+        ),
       ),
     );
   }
