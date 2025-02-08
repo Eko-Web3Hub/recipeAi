884d1da75ed30f1d5520669800f3fdf8b307f0f4
imdad <imdadadelabou0@gmail.com>
2025-02-08 07:46:04 +0100

    [FEAT] Test the updated ingredient method

--- lib/kitchen/presentation/ingredient_controller.dart
+++ lib/kitchen/presentation/ingredient_controller.dart
@@ -1,15 +1,47 @@
+import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:recipe_ai/auth/application/auth_user_service.dart';
+import 'package:recipe_ai/kitchen/domain/repositories/kitchen_inventory_repository.dart';
 
-abstract class IngredientState {}
+import '../../receipe/domain/model/ingredient.dart';
 
-class IngredientInitialState extends IngredientState {}
+abstract class IngredientState extends Equatable {}
 
-class IngredientUpdatedState extends IngredientState {}
+class IngredientInitialState extends IngredientState {
+  @override
+  List<Object?> get props => [];
+}
+
+class IngredientUpdatedState extends IngredientState {
+  IngredientUpdatedState(this.ingredient);
+
+  final Ingredient ingredient;
+
+  @override
+  List<Object?> get props => [ingredient];
+}
 
 class IngredientController extends Cubit<IngredientState> {
-  IngredientController() : super(IngredientInitialState());
+  IngredientController(
+    this.ingredient,
+    this._kitchenInventoryRepository,
+    this._authUserService,
+  ) : super(
+          IngredientInitialState(),
+        );
 
-  Future<void> updateIngredient(int quantity) async {
-    emit(IngredientInitialState());
+  final Ingredient ingredient;
+  final IKitchenInventoryRepository _kitchenInventoryRepository;
+  final IAuthUserService _authUserService;
+
+  Future<void> updateIngredient(String quantity) async {
+    final uid = _authUserService.currentUser!.uid;
+    final updatedIngredient = ingredient.copy(
+      quantity: quantity,
+    );
+
+    await _kitchenInventoryRepository.save(uid, updatedIngredient);
+
+    emit(IngredientUpdatedState(updatedIngredient));
   }
 }
