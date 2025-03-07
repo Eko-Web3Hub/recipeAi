3b6c4d6abe5273388b7bed4a8a57378c949c3c3d
imdad <imdadadelabou0@gmail.com>
2025-03-07 22:27:34 +0100

    [FEAT] change icon

--- lib/nav/splash_screen.dart
+++ lib/nav/splash_screen.dart
@@ -33,7 +33,7 @@ class _SplashScreenState extends State<SplashScreen> {
     return Scaffold(
       body: Center(
         child: Image.asset(
-          'assets/images/logo.png',
+          'assets/images/newLogo.png',
         ),
       ),
     );
