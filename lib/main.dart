da0c37937693742115b1b27863857b83c24b1092
imdad <imdadadelabou0@gmail.com>
2025-11-17 21:51:38 +0100

    [FEAT] handle received notification

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
@@ -44,6 +45,14 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+void _handleBackgroundNotificationTap() {
+  FirebaseMessaging.onMessageOpenedApp.listen(_handleNotificationReceived);
+}
+
+void _handleNotificationReceived(RemoteMessage notification) {
+  log('Notification received: ${notification.toString()}');
+}
+
 class _MyAppState extends State<MyApp> {
   late GoRouter _router;
 
@@ -52,6 +61,7 @@ class _MyAppState extends State<MyApp> {
     super.initState();
 
     _router = createRouter();
+    _handleBackgroundNotificationTap();
   }
 
   @override
