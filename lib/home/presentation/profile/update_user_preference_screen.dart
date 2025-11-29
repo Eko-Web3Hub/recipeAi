343b601839c8607558ad688d67caeac4d4dcf59a
imdad <imdadadelabou0@gmail.com>
2025-11-29 07:51:55 +0100

    [FEAT] improve the user updateUserPreferenceScreen by changing the app bar widget

--- lib/home/presentation/profile/update_user_preference_screen.dart
+++ lib/home/presentation/profile/update_user_preference_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:recipe_ai/di/container.dart';
+import 'package:recipe_ai/kitchen/presentation/kitchen_inventory_screen.dart';
 import 'package:recipe_ai/user_account/presentation/translation_controller.dart';
 import 'package:recipe_ai/user_preferences/presentation/user_preference_update_widget.dart';
-import 'package:recipe_ai/utils/styles.dart';
 
 class UpdateUserPreferenceScreen extends StatelessWidget {
   const UpdateUserPreferenceScreen({super.key});
@@ -13,15 +14,13 @@ class UpdateUserPreferenceScreen extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        surfaceTintColor: Colors.transparent,
-        title: Text(
-          appTexts.updateUserPreference,
-          style: appBarTextStyle,
-        ),
+      appBar: KitchenInventoryAppBar(
+        title: appTexts.updateUserPreference,
+        arrowLeftOnPressed: () => context.pop(),
       ),
-      body: const UserPreferenceUpdateWidget(),
+      body: SafeArea(
+        child: const UserPreferenceUpdateWidget(),
+      ),
     );
   }
 }
